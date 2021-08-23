5467c0c97eedef81199f8a77d09b6df3391ea971
alin <alin@exome.co>
2021-08-23 21:20:46 +0800

    chore: update

---
+++ packages/components/rollup.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies,global-require */
+/* eslint-disable import/no-extraneous-dependencies,global-require */
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
+import css from "rollup-plugin-import-css";
+import includePaths from "rollup-plugin-includepaths";
+import commonjs from "rollup-plugin-commonjs";
+// import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const pkg = require("./package.json");
+
+export default {
+  input: "src/index.ts",
+  output: [
+    { file: pkg.main, format: "cjs", sourcemap: true },
+    { file: pkg.module, format: "esm", sourcemap: true },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    css(),
+    typescript({ module: "CommonJS", typescript: require("typescript") }),
+    // nodeResolve(),
+    commonjs({ extensions: [".js", ".ts"] }),
+    includePaths({ paths: ["./"] }),
+  ],
+};
---
+++ packages/components/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  Button
+} from "./Button";
+export {
+  Header
+} from "./Header";
+export {
+  Page
+} from "./Page"
