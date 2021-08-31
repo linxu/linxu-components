d1bd0c28600166112134bf97b2d9679f5ff52e4e
alin <alin@exome.co>
2021-08-31 10:04:57 +0800

    chore: update

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,5 @@
 export { Button, ExomeButton, ButtonProps } from "Components/Button/Button";
 export { Text, ExomeText, TextProps } from "Components/Text/Text";
 export { Box, ExomeBox, BoxProps } from "Components/Box/Box";
+export { Line, ExomeLine, LineProps } from "Components/Line/Line";
 export { getTheme } from "theme/getTheme";
